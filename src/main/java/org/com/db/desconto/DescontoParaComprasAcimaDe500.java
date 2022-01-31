307e36bbbd27b0eb2e80387aef5f9cd793beb331
Lelton Borges <leltondev@gmail.com>
2022-01-31 20:35:56 -0300

    Add pattern Template Method

--- src/main/java/org/com/db/desconto/DescontoParaComprasAcimaDe500.java
+++ src/main/java/org/com/db/desconto/DescontoParaComprasAcimaDe500.java
@@ -4,18 +4,19 @@ import org.com.db.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class DescontoParaComprasAcimaDe500 extends Desconto{
+public class DescontoParaComprasAcimaDe500 extends Desconto {
 
     public DescontoParaComprasAcimaDe500(Desconto next) {
         super(next);
     }
 
-    public BigDecimal calcular(Orcamento orcamento){
-        if(orcamento.getValue().compareTo(BigDecimal.valueOf(500)) > 0){
-            return orcamento.getValue().multiply(BigDecimal.valueOf(0.05));
-        }
+    public BigDecimal efetuarCalculo(Orcamento orcamento) {
+        return orcamento.getValue().multiply(new BigDecimal("0.05"));
+    }
 
-        return next.calcular(orcamento);
+    @Override
+    public boolean deveAplicar(Orcamento orcamento) {
+        return orcamento.getValue().compareTo(BigDecimal.valueOf(500)) > 0;
     }
 
 }
--- src/main/java/org/com/db/desconto/DescontoParaMaisDeCincoItens.java
+++ src/main/java/org/com/db/desconto/DescontoParaMaisDeCincoItens.java
@@ -4,17 +4,19 @@ import org.com.db.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class DescontoParaMaisDeCincoItens extends Desconto{
+public class DescontoParaMaisDeCincoItens extends Desconto {
 
     public DescontoParaMaisDeCincoItens(Desconto next) {
         super(next);
     }
 
-    public BigDecimal calcular(Orcamento orcamento){
-        if(orcamento.getQuantity() > 5){
-            return orcamento.getValue().multiply(BigDecimal.valueOf(0.1));
-        }
-        return next.calcular(orcamento);
+    public BigDecimal efetuarCalculo(Orcamento orcamento) {
+        return orcamento.getValue().multiply(BigDecimal.valueOf(0.1));
+    }
+
+    @Override
+    public boolean deveAplicar(Orcamento orcamento) {
+        return orcamento.getQuantity() > 5;
     }
 
 }
